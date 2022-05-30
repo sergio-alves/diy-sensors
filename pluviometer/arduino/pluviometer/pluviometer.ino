b14725d5515452e8e931e86fd5ec2d7dde306e57
Sergio Alves <sergio.alvesutopix.ch>
2022-05-30 17:50:38 +0200

    added M	QTT support

--- pluviometer/arduino/pluviometer/pluviometer.ino
+++ pluviometer/arduino/pluviometer/pluviometer.ino
@@ -1,8 +1,9 @@
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <EEPROM.h>
 #include <ArduinoJson.h>
-
+#include <WiFiUdp.h>
 
 const char* ap_ssid     = "Pluviometer-AP";
 
@@ -29,6 +30,9 @@ struct {
 
 WiFiClient espClient;
 PubSubClient client;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
 
 /**
  * Initializes the global configuration stored in EEPROM
@@ -146,6 +150,9 @@ void setup_mqtt_client() {
     client.subscribe(&(data.mqtt.in_topic[0]));    
 }
 
+/**
+ * Setup app after wake up
+ */
 void setup() {
   // put your main code here, to run repeatedly:
   Serial.begin(115200);
@@ -153,6 +160,8 @@ void setup() {
 
   // Wait for serial to initialize.
   while(!Serial) { }
+
+  timeClient.begin();
 
   // load configuration
   EEPROM.begin(sizeof(data));
@@ -172,16 +181,65 @@ void setup() {
   setup_mqtt_client();
 }
 
+/**
+ * Attempt to reconnect to mqtt server. 
+ */
+void reconnectMqtt() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(data.mqtt.out_topic, "{\"status\":\"online\"}");
+      client.subscribe(data.mqtt.in_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
+/* PI * 5^2 cm */
+#define COLLECTOR_SURFACE 78,539816339744830961566084581988
+#define SQARE_METER_IN_SQUARE_CENTIMETERS 10000
+
+/* The bucket volume in ml */
+#define BUCKET_VOLUME_PER_TICK 8
+
+/**
+ * Attempt mqtt transmission of data
+ */
+void transmitToMQTT() {
+    /* calculates the mm/L per square meter equivalent */
+    DynamicJsonDocument doc(1024);
+    doc["ticks"]=data.ticks;
+    doc["mm_per_sqm"]=BUCKET_VOLUME_PER_TICK * SQARE_METER_IN_SQUARE_CENTIMETERS / COLLECTOR_SURFACE;
+    doc["rain_start"]=
+}
 
 void loop() {
-  //mqtt
-  client.loop(); 
+  // mqtt get messages
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   
+  timeClient.update();
+  
+  // increment the tickets and store into eeprom
   data.ticks++;
   saveDataAndConfiguration();
 
-  
+  // transmit data to mqtt
+  transmitToMQTT();
   
   // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
   Serial.println("I'm awake, but I'm going into deep sleep mode for 30 seconds");
